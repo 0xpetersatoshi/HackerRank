8f8ba89ad65e6c81da1f07b8eed98db26521509b
pbegle <peterbegle@gmail.com>
2018-09-08 13:42:04 -0700

    Adding solution to minimum swaps problem.

---
+++ arrays/minimum_swaps.py
@@ -0,0 +1,43 @@
+# Minimum Swaps 2
+
+def minimum_swaps(arr):
+    """Returns the minimum number of swaps to re-oder array in ascending order."""
+    
+    # Create dict to hold index position and
+    # Create a sorted array variable
+    index_of = {value: index for index, value in enumerate(arr)}
+    sorted_arr = sorted(arr)
+
+    swaps = 0
+    for i in reversed(sorted_arr):
+
+        # Find index of i in original array
+        arr_index = index_of[i]
+
+        # Find sorted index
+        sorted_index = sorted_arr.index(i)
+
+        if arr_index != sorted_index:
+            
+            # Perform swap if indexes don't match
+            arr[arr_index], arr[sorted_index] = arr[sorted_index], arr[arr_index]
+            
+            # Increment swaps variable
+            swaps += 1
+
+            # Update to reflect new index positions    
+            index_of.update( {i: sorted_index} )
+            index_of.update( {arr[arr_index]: arr_index})
+
+    return swaps
+
+
+# 4 3 2 1 
+arr = [2, 3, 4, 1, 5]
+arr2 = [1, 3, 5, 2, 4, 6, 8]
+
+
+if __name__ == "__main__":
+
+    swaps = minimum_swaps(arr)
+    print(swaps)
